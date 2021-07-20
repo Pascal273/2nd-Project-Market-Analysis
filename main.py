28a10b85573d433faab06848f7bbc757eb1227c4
Pascal <77117642+Pascal273@users.noreply.github.com>
2021-07-20 20:55:39 +0200

    Takes each category and writes CSV files with all details per category.

--- main.py
+++ main.py
@@ -4,61 +4,105 @@ from time import sleep
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-site_url = "https://books.toscrape.com/index.html"
-all_data = []
+
+def scrape_categories(url):
+    category_links = {}
+    while not category_links:
+        try:
+            r = requests.get(url)
+            doc = BeautifulSoup(r.text.encode(r.encoding), "html.parser")
+
+            for category in doc.select_one(".nav-list ul").select("li a"):
+                category_name = category.text.strip()
+                category_page_link = category["href"]
+                category_page_url = urljoin(url, category_page_link)
+                category_links[category_name] = category_page_url
+
+        except requests.exceptions.ConnectionError:
+            print("ConnectionError occurred in scrape_categories!\nWaiting 3 seconds to reconnect...")
+            sleep(3)
+
+    return category_links
 
 
-def scrape(url):
-    try:
-        r = requests.get(url)
-        print(url)
-        doc = BeautifulSoup(r.text, "html.parser")
-        for product in doc.select(".product_pod"):
-            product_page_link = product.select_one(".image_container").select_one("a")["href"]
-            product_page_url = urljoin(url, product_page_link)
-            all_data.append(scrape_details(product_page_url))
+def scrape_products(category_url):
+    product_urls = []
+    while category_url != "":
+        try:
+            r = requests.get(category_url)
+            doc = BeautifulSoup(r.text.encode(r.encoding), "html.parser")
+            for product in doc.select(".product_pod"):
+                product_page_link = product.select_one(".image_container").select_one("a")["href"]
+                product_page_url = urljoin(category_url, product_page_link)
+                product_urls.append(product_page_url)
 
-        next_button = doc.select_one(".next")
-        if next_button:
-            next_href = next_button.a["href"]
-            next_href = urljoin(url, next_href)
-            url = next_href
-            scrape(url)
+            next_button = doc.select_one(".next")
+            if next_button:
+                next_href = next_button.a["href"]
+                next_href = urljoin(category_url, next_href)
+                category_url = next_href
+            else:
+                category_url = ""
 
-    except requests.exceptions.ConnectionError:
-        print("requests.exceptions.ConnectionError occurred!\nWaiting 3 seconds to reconnect...")
-        sleep(3)
-        scrape(url)
+        except requests.exceptions.ConnectionError:
+            print("ConnectionError occurred in scrape_from_category!\nWaiting 3 seconds to reconnect...")
+            sleep(3)
+
+    return product_urls
 
 
 def scrape_details(product_url):
-    r = requests.get(product_url)
-    doc = BeautifulSoup(r.text, "html.parser")
-    prod_category = doc.select(".breadcrumb a")[-1].text
-    for detail in doc.select(".product_page"):
-        prod_title = detail.select_one(".product_main").select_one("h1").text
-        prod_review_rating = detail.select_one(".star-rating").attrs["class"][1]
-        prod_img_link = detail.select_one(".thumbnail").img["src"]
-        prod_img_url = urljoin(site_url, prod_img_link)
-        prod_description = detail.find("p", class_=False).text
-        prod_info_table = detail.select_one(".table-striped").select("tr")
-        prod_details = {
-            prod_title: {
-                "Product_Page_URL": product_url,
+    prod_details = {}
+    while not prod_details:
+        try:
+            r = requests.get(product_url)
+            doc = BeautifulSoup(r.text.encode(r.encoding), "html.parser")
+            prod_category = doc.select(".breadcrumb a")[-1].text
+            details = doc.select_one(".product_page")
+            prod_title = details.select_one(".product_main").select_one("h1").text
+            prod_review_rating = details.select_one(".star-rating").attrs["class"][1]
+            prod_img_link = details.select_one(".thumbnail").img["src"]
+            prod_img_url = urljoin(site_url, prod_img_link)
+            prod_description = details.find("p", class_=False)
+            if prod_description is None:
+                prod_description = "Description currently not available"
+            else:
+                prod_description = prod_description.text
+            prod_info_table = details.select_one(".table-striped").select("tr")
+            prod_details = {
+                "Title": prod_title,
+                "Product Page URL": product_url,
                 "Description": prod_description,
                 "Category": prod_category,
-                "Image_URL": prod_img_url,
-                "Review Rating": f"{prod_review_rating} Stars"}}
-        for tr in prod_info_table:
-            keyword = tr.select_one("th").text
-            value = tr.select_one("td").text
-            prod_details[prod_title][keyword] = value
+                "Image URL": prod_img_url,
+                "Review Rating": f"{prod_review_rating} Star(s) out of five"
+            }
+            for tr in prod_info_table:
+                keyword = tr.select_one("th").text
+                value = tr.select_one("td").text
+                prod_details[keyword] = value
+
+        except requests.exceptions.ConnectionError:
+            print("ConnectionError occurred in scrape_details!\nWaiting 3 seconds to reconnect...")
+            sleep(3)
 
     return prod_details
 
 
-print(scrape_details("https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"))
-print(scrape_details("https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"))
-# scrape(site_url)
+site_url = "https://books.toscrape.com/index.html"
+categories = scrape_categories(site_url)
+print(f"\n{len(categories)} categories found...")
 
+header = ["Title", "Product Page URL", "UPC", "Price (excl. tax)",
+          "Price (incl. tax)", "Availability", "Description",
+          "Category", "Review Rating", "Image URL"]
 
+for (cat_name, cat_link) in categories.items():
+    print(f"\nCurrent category: {cat_name}\n")
+    with open(f"Created CSV Files/{cat_name}.csv", "w", newline="", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=header)
+        writer.writeheader()
+        for book in scrape_products(cat_link):
+            detail = scrape_details(book)
+            writer.writerow({i: detail[i] for i in header})
+            print(f"{detail['Title']} analysed")
